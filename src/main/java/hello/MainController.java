3e2bc1147f3af3f17b21ea20182cf72c627b7706
Douglas Fernandes <douglas16.2009@gmail.com>
2017-04-14 23:18:09 -0300

    Heroku and PostgreSQL configuration testing

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -2,7 +2,6 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,7 +10,7 @@ import hello.User;
 import hello.UserRepository;
 
 @Controller    // Indica que esta classe é um Controller.
-@RequestMapping(path="/crud") // Faz com que a URL inicia com "/crud" (após o caminho da aplicação/domínio).
+// @RequestMapping(path="/crud") // Faz com que a URL inicia com "/crud" (após o caminho da aplicação/domínio).
 public class MainController {
 	@Autowired // Chama o Bean de nome userRepository,
 	           // O qual é gerado automaticamente pelo Spring, nós o usaremos para manipular os dados.
@@ -38,20 +37,20 @@ public class MainController {
 		return userRepository.findAll();
 	}
 	
-	@GetMapping(path="/getById")
+	@GetMapping(path="/find")
 	public @ResponseBody User getUserById(Long id) {
 		// Isto retorna um usuário com a ID especificada.
-		return userRepository.findById(id);
+		return userRepository.findOne(id);
 	}
 	
-	@GetMapping(path="/deleteById")
+	@GetMapping(path="/delete")
 	public @ResponseBody String deleteUserById(Long id) {
 		// Isto deleta um usuário/registro com a ID especificada.
 		userRepository.delete(id);
 		return MsgsConstantes.APAGADO;
 	}
 	
-	@GetMapping(path="/updateById")
+	@GetMapping(path="/update")
 	public @ResponseBody String updateUserById(User user) {
 		if (user.getId() == null) {
 			return "Erro";
--- src/main/java/hello/UserRepository.java
+++ src/main/java/hello/UserRepository.java
@@ -8,12 +8,5 @@ import hello.User;
 // CRUD refers Create, Read, Update, Delete.
 
 public interface UserRepository extends CrudRepository<User, Long/*Integer*/> {
-	/**
-	 * Este método é responsável por listar um usuário por sua id
-	 * @param id
-	 * @return
-	 */
-	public User findById(Long id);
-	// Equivalente à consulta SQL: SELECT * FROM User WHERE id = ?;
-	 
+	
 }
