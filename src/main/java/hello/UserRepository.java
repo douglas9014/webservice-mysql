548c4529e3584d24f630081fd15a8f563fe1e0cd
Douglas Fernandes <douglas16.2009@gmail.com>
2017-04-18 03:44:39 -0300

    Branch problems

--- src/main/java/hello/UserRepository.java
+++ src/main/java/hello/UserRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package hello;
 
 import org.springframework.data.repository.CrudRepository;
@@ -11,17 +10,3 @@ import hello.User;
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
-=======
-package hello;
-
-import org.springframework.data.repository.CrudRepository;
-
-import hello.User;
-
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete.
-
-public interface UserRepository extends CrudRepository<User, Long/*Integer*/> {
-	
-}
->>>>>>> 132ca0a0187f519ec563c15f60a9629b9d986fb9
